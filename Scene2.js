a3bf67503af2759a1dcaf531974de1b5a919c36b
ThGnommy <th.brandoli@gmail.com>
2020-05-04 16:12:15 +0200

    fix bounce player + floor

--- Scene2.js
+++ Scene2.js
@@ -11,21 +11,25 @@ cursors;
 key_A;
 key_D;
 
+floors;
+collider;
 walls;
 ball;
 player;
 player_2;
 
 preload() {
-
     this.load.image('wall_h', 'assets/wall_horizzontal.png')
     this.load.image('wall_v', 'assets/wall_vertical.png')
     this.load.image('wall_m', 'assets/wall_middle.png')
     this.load.image('player1', "assets/player_2.png");
     this.load.image('ball', "assets/ball.png")
     this.load.image('player2', "assets/player_2.png");
-}
 
+    this.load.image('floor_left', 'assets/floor_left.png')
+    this.load.image('floor_right', 'assets/floor_right.png')
+
+}
 
 
 create() {
@@ -35,12 +39,17 @@ create() {
     this.key_D = this.input.keyboard.addKey('D');
 
     this.walls = this.physics.add.staticGroup();
-    this.walls.create(400, 580, 'wall_h');
     this.walls.create(400, 20, 'wall_h');
     this.walls.create(20, 300, 'wall_v');
     this.walls.create(780, 300, 'wall_v');
     this.walls.create(400, 450, 'wall_m');
 
+    this.floors = this.physics.add.staticGroup();
+    this.floors.create(220, 580, 'floor_left');
+    this.floors.create(580, 580, 'floor_right');
+
+    
+    
     // Player 1
     this.player = this.physics.add.image(100, 450, 'player1');
 
@@ -55,10 +64,11 @@ create() {
     this.ball.body.setAllowGravity(false);
     
     // Physics 1 Settings
-
+    
     this.physics.add.collider(this.player, this.walls)
     this.physics.add.collider(this.ball, this.walls)
     this.physics.add.collider(this.player, this.ball)
+    this.physics.add.collider(this.player, this.floors)
 
     // Physics 2 Settings
 
@@ -66,22 +76,29 @@ create() {
 
     this.physics.add.collider(this.ball, this.walls)
     this.physics.add.collider(this.player_2, this.ball)
+    this.physics.add.collider(this.player_2, this.floors)
 
     this.ballStart = this.time.delayedCall(2000, this.BallStart, [], this)
-
 }
 
 update ()
 {
-    let rn_x = Phaser.Math.Between(-160, 160);
+    let rn_x1 = Phaser.Math.Between(30, 200);
+    let rn_x2 = Phaser.Math.Between(-200, -30)
     let rn_y = Phaser.Math.Between(-250, -500);
 
     this.Player1Input();
     this.Player2Input();
 
-    this.BallAngleBounce(rn_x, rn_y);
+    this.BallAngleBounceP1(rn_x1, rn_y);
+    this.BallAngleBounceP2(rn_x2, rn_y);
+
+    this.physics.add.overlap(this.ball, this.floors, this.CheckCollision, [], this);
 }
 
+CheckCollision() {
+    console.log("Collide")
+}
 
 BallStart() {
     let rn_x = Phaser.Math.Between(-160, 160);
@@ -117,12 +134,16 @@ Player2Input() {
     }
 }
 
-BallAngleBounce(x, y) {
+BallAngleBounceP1(x, y) {
     if(this.player.body.touching.up) {
         this.ball.setVelocityX(x)
         this.ball.setVelocityY(y)
     }
 
+
+}
+
+BallAngleBounceP2(x, y) {
     if(this.player_2.body.touching.up) {
         this.ball.setVelocityX(x)
         this.ball.setVelocityY(y)
